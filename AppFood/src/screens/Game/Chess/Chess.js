27f04ecbc3a0c98dc4a5bd20688fdc67e61a5da3
danghung97 <danghungdao1997@gmail.com>
2019-12-19 09:32:00 +0700

    update chess

--- AppFood/src/screens/Game/Chess/Chess.js
+++ AppFood/src/screens/Game/Chess/Chess.js
@@ -1,13 +1,18 @@
 
-export const findValidRoad = (piece, from, chessBoard, color) => {
-  let validMove=[]
+export const FindValidRoad = (piece, from, ColorBoard, chessBoard, color) => {
   const posX = from.posX
   const posY = from.posY
+  ColorBoard[posX][posY] = '#696969'
+  let valid=[{posX, posY}]
+  let canEats = []
   // con thieu truong hop thăng cấp của pawn 
+  // thieu swap queen va rook
+  // thieu check chieu tuong,
+  // thieu check het co
   switch (piece) {
     case 'P': // Pawn
       let maxMoves = 1 // max number of pawn moves
-      posXValidMoves = posX
+      posXvalid = posX
       if (color === 'b') {
         if (posX === 1) { // if this is first step of pawn, pawn can go 2 moves
           maxMoves = 2
@@ -16,8 +21,21 @@ export const findValidRoad = (piece, from, chessBoard, color) => {
           if (!!chessBoard[posX + i][posY]) {
             break
           }
-          posXValidMoves = posX + i
+          posXvalid = posX + i
+          ColorBoard[posXvalid][posY] = '#696969'
+          valid.push({'posX': posXvalid, 'posY': posY})
         }
+        //check can eats
+        if (!!chessBoard[posX + 1][posY - 1] && chessBoard[posX + 1][posY - 1][0] !== color) {
+          ColorBoard[posX + 1][posY - 1] = '#696969'
+          valid.push({'posX': posX + 1, 'posY': posY - 1})
+          canEats.push({'posX': posX + 1, 'posY': posY - 1})
+        }
+        if (!!chessBoard[posX + 1][posY + 1] && chessBoard[posX + 1][posY + 1][0] !== color) {
+          ColorBoard[posX + 1][posY + 1] = '#696969'
+          valid.push({'posX': posX + 1, 'posY': posY + 1})
+          canEats.push({'posX': posX + 1, 'posY': posY + 1})
+        } 
       } else {
         if (posX === 6) { // if this is first step of pawn, pawn can go 2 moves
           maxMoves = 2
@@ -26,81 +44,252 @@ export const findValidRoad = (piece, from, chessBoard, color) => {
           if (!!chessBoard[posX - i][posY]) {
             break
           }
-          posXValidMoves = posX - i
+          posXvalid = posX - i
+          ColorBoard[posXvalid][posY] = '#696969'
+          valid.push({'posX': posXvalid, 'posY': posY})
         }
+        if (!!chessBoard[posX - 1][posY - 1] && chessBoard[posX - 1][posY - 1][0] !== color) {
+          ColorBoard[posX - 1][posY - 1] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY - 1})
+          canEats.push({'posX': posX - 1, 'posY': posY - 1})
+        }
+        if (!!chessBoard[posX - 1][posY + 1] && chessBoard[posX - 1][posY + 1][0] !== color) {
+          ColorBoard[posX - 1][posY + 1] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY + 1})
+          canEats.push({'posX': posX - 1, 'posY': posY + 1})
+        } 
       }
-      
-      validMove.push({'posX': posXValidMoves, 'posY': posY})
-      return validMove
+      return {canEats, valid, ColorBoard}
     case 'R': // Rook
-      return findStraight(posX, posY, chessBoard)
+      return findStraight(posX, posY, ColorBoard, chessBoard, canEats)
     case 'B': // Bishop
-      return findDiagonal(posX, posY, chessBoard)
-    case 'K': // Knight
+      return findDiagonal(posX, posY, ColorBoard, chessBoard, canEats)
+    case 'N': // 
       // Knight has max 8 position can moves
 
       //Left
-      if (posX - 2 >= 0 && posY - 1 >=0 && !chessBoard[posX - 2][posY - 1]) { // 1
-        validMove.push({'posX': posX - 2, 'posY': posY - 1})
+      if (posX - 2 >= 0 && posY - 1 >=0) { // 1
+        if (!chessBoard[posX - 2][posY - 1]) {
+          ColorBoard[posX - 2][posY - 1] = '#696969'
+          valid.push({'posX': posX - 2, 'posY': posY - 1})
+        } else {
+          if(chessBoard[posX - 2][posY - 1][0] !== color) {
+            canEats.push({'posX': posX - 2, 'posY': posY - 1})
+            ColorBoard[posX - 2][posY - 1] = '#696969'
+            valid.push({'posX': posX - 2, 'posY': posY - 1})
+          }
+        }
       }
-      if (posX - 1 >= 0 && posY - 2 >= 0 && !chessBoard[posX - 1][posY - 2]) { // 2
-        validMove.push({'posX': posX - 1, 'posY': posY - 2})
+      if (posX - 1 >= 0 && posY - 2 >= 0) { // 2
+        if(!chessBoard[posX - 1][posY - 2]) {
+          ColorBoard[posX - 1][posY - 2] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY - 2})
+        } else {
+          if(chessBoard[posX - 1][posY - 2][0] !== color) {
+            canEats.push({'posX': posX - 1, 'posY': posY - 2})
+            ColorBoard[posX - 1][posY - 2] = '#696969'
+            valid.push({'posX': posX - 1, 'posY': posY - 2})
+          }
+        }
+       
       }
-      if (posX + 2 < 8 && posY - 1 >= 0 && !chessBoard[posX + 2][posY - 1]) { // 3
-        validMove.push({'posX': posX + 2, 'posY': posY - 1})
+      if (posX + 2 < 8 && posY - 1 >= 0) { // 3
+        if (!chessBoard[posX + 2][posY - 1]) {
+          ColorBoard[posX + 2][posY - 1] = '#696969'
+          valid.push({'posX': posX + 2, 'posY': posY - 1})
+        } else {
+          if (chessBoard[posX + 2][posY - 1][0] !== color) {
+            canEats.push({'posX': posX + 2, 'posY': posY - 1})
+            ColorBoard[posX + 2][posY - 1] = '#696969'
+            alid.push({'posX': posX + 2, 'posY': posY - 1})
+          }
+        }
+        
       }
-      if (posX + 1 < 8 && posY - 2 >= 0 && !chessBoard[posX + 1][posY - 2]) { // 4
-        validMove.push({'posX': posX + 1, 'posY': posY - 2})
+      if (posX + 1 < 8 && posY - 2 >= 0) { // 4
+        if(!chessBoard[posX + 1][posY - 2]) {
+          ColorBoard[posX + 1][posY - 2] = '#696969'
+          valid.push({'posX': posX + 1, 'posY': posY - 2})
+        } else {
+          if (chessBoard[posX + 1][posY - 2][0] !== color) {
+            canEats.push({'posX': posX + 1, 'posY': posY - 2})
+            ColorBoard[posX + 1][posY - 2] = '#696969'
+            valid.push({'posX': posX + 1, 'posY': posY - 2})
+          }
+        }
+        
       }
       // Right
-      if (posX - 2 >= 0 && posY + 1 < 8 && !chessBoard[posX - 2][posY + 1]) { // 5
-        validMove.push({'posX': posX - 2, 'posY': posY + 1})
+      if (posX - 2 >= 0 && posY + 1 < 8) { // 5
+        if(!chessBoard[posX - 2][posY + 1]){
+          ColorBoard[posX - 2][posY + 1] = '#696969'
+          valid.push({'posX': posX - 2, 'posY': posY + 1})
+        } else {
+          if(chessBoard[posX - 2][posY + 1][0] !== color) {
+            canEats.push({'posX': posX - 2, 'posY': posY + 1})
+            ColorBoard[posX - 2][posY + 1] = '#696969'
+            valid.push({'posX': posX - 2, 'posY': posY + 1})
+          }
+        }
+        
       }
-      if (posX - 1 >= 0 && posY + 2 < 8 && !chessBoard[posX - 1][posY + 2]) { // 6
-        validMove.push({'posX': posX - 1, 'posY': posY + 2})
+      if (posX - 1 >= 0 && posY + 2 < 8) { // 6
+        if(!chessBoard[posX - 1][posY + 2]) {
+          ColorBoard[posX - 1][posY + 2] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY + 2})
+        } else {
+          if (chessBoard[posX - 1][posY + 2][0] !== color) {
+            canEats.push({'posX': posX - 1, 'posY': posY + 2})
+            ColorBoard[posX - 1][posY + 2] = '#696969'
+            valid.push({'posX': posX - 1, 'posY': posY + 2})
+          }
+        }
+       
       }
-      if (posX + 2 < 8 && posY + 1 < 8 && !chessBoard[posX + 2][posY + 1]) { // 7
-        validMove.push({'posX': posX + 2, 'posY': posY + 1})
+      if (posX + 2 < 8 && posY + 1 < 8) { // 7
+        if (!chessBoard[posX + 2][posY + 1]) {
+          ColorBoard[posX + 2][posY + 1] = '#696969'
+          valid.push({'posX': posX + 2, 'posY': posY + 1})
+        } else {
+          if (chessBoard[posX + 2][posY + 1][0] !== color) {
+            canEats.push({'posX': posX + 2, 'posY': posY + 1})
+            ColorBoard[posX + 2][posY + 1] = '#696969'
+            valid.push({'posX': posX + 2, 'posY': posY + 1})
+          }
+        }
+       
       }
-      if (posX + 1 < 8 && posY + 2 < 8 && !chessBoard[posX + 1][posY + 2]) { // 8
-        validMove.push({'posX': posX + 1, 'posY': posY + 2})
+      if (posX + 1 < 8 && posY + 2 < 8) { // 8
+        if (!chessBoard[posX + 1][posY + 2]) {
+          ColorBoard[posX + 1][posY + 2] = '#696969'
+          valid.push({'posX': posX + 1, 'posY': posY + 2})
+        } else {
+          if (chessBoard[posX + 1][posY + 2][0] !== color) {
+            canEats.push({'posX': posX + 1, 'posY': posY + 2})
+            ColorBoard[posX + 1][posY + 2] = '#696969'
+            valid.push({'posX': posX + 1, 'posY': posY + 2})
+          }
+        }
+        
       }
-      return validMove
+      return {canEats, valid, ColorBoard}
 
     case 'K': // King
-      if ( posX + 1 < 8 && !chessBoard[posX + 1][posY]) { // top
-        validMove.push({'posX': posX + 1, 'posY': posY})
+      if ( posX + 1 < 8) { // top
+        if (!chessBoard[posX + 1][posY]) {
+          ColorBoard[posX + 1][posY] = '#696969'
+          valid.push({'posX': posX + 1, 'posY': posY})
+        } else {
+          if (chessBoard[posX + 1][posY][0] !== color) {
+            canEats.push({'posX': posX + 1, 'posY': posY})
+            ColorBoard[posX + 1][posY] = '#696969'
+            valid.push({'posX': posX + 1, 'posY': posY})
+          }
+        }
       }
-      if ( posX - 1 >= 0 && !chessBoard[posX - 1][posY]) { // bot
-        validMove.push({'posX': posX - 1, 'posY': posY})
+      if ( posX - 1 >= 0) { // bot
+        if(!chessBoard[posX - 1][posY]) {
+          ColorBoard[posX - 1][posY] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY})
+        } else {
+          if (chessBoard[posX - 1][posY][0] !== color) {
+            canEats.push({'posX': posX - 1, 'posY': posY})
+            ColorBoard[posX - 1][posY] = '#696969'
+            valid.push({'posX': posX - 1, 'posY': posY})
+          }
+        }
+        
       }
-      if ( posY - 1 >= 0 && !chessBoard[posX][posY - 1]) { // left 
-        validMove.push({'posX': posX - 1, 'posY': posY - 1})
+      if ( posY - 1 >= 0) { // left 
+        if (!chessBoard[posX][posY - 1]) {
+          ColorBoard[posX][posY - 1] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY - 1})
+        } else {
+          if(chessBoard[posX][posY - 1][0] !== color) {
+            canEats.push({'posX': posX - 1, 'posY': posY - 1})
+            ColorBoard[posX][posY - 1] = '#696969'
+            valid.push({'posX': posX - 1, 'posY': posY - 1})
+          }
+        }
+        
       }
-      if ( posY + 1 < 8 && !chessBoard[posX][posY + 1]) { // right
-        validMove.push({'posX': posX - 1, 'posY': posY + 1})
+      if ( posY + 1 < 8) { // right
+        if (!chessBoard[posX][posY + 1]) {
+          ColorBoard[posX][posY + 1] = '#696969'
+          valid.push({'posX': posX, 'posY': posY + 1})
+        } else {
+          if (chessBoard[posX][posY + 1][0] !== color) {
+            canEats.push({'posX': posX, 'posY': posY + 1})
+            ColorBoard[posX][posY + 1] = '#696969'
+            valid.push({'posX': posX, 'posY': posY + 1})
+          }
+        }
+        
       }
-      if ( posX - 1 >= 0 && posX - 1 >= 0 && !chessBoard[posX - 1][posY - 1]) { 
-        validMove.push({'posX': posX - 1, 'posY': posY - 1})
+      if ( posX - 1 >= 0 && posX - 1 >= 0) {
+        if (!chessBoard[posX - 1][posY - 1]) {
+          ColorBoard[posX - 1][posY - 1] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY - 1})
+        } else {
+          if (chessBoard[posX - 1][posY - 1][0] !== color) {
+            canEats.push({'posX': posX - 1, 'posY': posY - 1})
+            ColorBoard[posX - 1][posY - 1] = '#696969'
+            valid.push({'posX': posX - 1, 'posY': posY - 1})
+          }
+        }
+        
       }
-      if ( posX + 1 < 8 && posX + 1 <8 && !chessBoard[posX + 1][posY + 1]) { 
-        validMove.push({'posX': posX + 1, 'posY': posY + 1})
+      if ( posX + 1 < 8 && posX + 1 <8) {
+        if (!chessBoard[posX + 1][posY + 1]) {
+          ColorBoard[posX + 1][posY + 1] = '#696969'
+          valid.push({'posX': posX + 1, 'posY': posY + 1})
+        } else {
+          if (chessBoard[posX + 1][posY + 1][0] !== color) {
+            canEats.push({'posX': posX + 1, 'posY': posY + 1})
+            ColorBoard[posX + 1][posY + 1] = '#696969'
+            valid.push({'posX': posX + 1, 'posY': posY + 1})
+          }
+        }
+        
       }
-      if ( posX - 1 >= 0 && posY + 1 < 8 && !chessBoard[posX - 1][posY + 1]) { 
-        validMove.push({'posX': posX - 1, 'posY': posY + 1})
+      if ( posX - 1 >= 0 && posY + 1 < 8) {
+        if (!chessBoard[posX - 1][posY + 1]) {
+          ColorBoard[posX - 1][posY + 1] = '#696969'
+          valid.push({'posX': posX - 1, 'posY': posY + 1})
+        } else {
+          if (chessBoard[posX - 1][posY + 1][0] !== color) {
+            canEats.push({'posX': posX - 1, 'posY': posY + 1})
+            ColorBoard[posX - 1][posY + 1] = '#696969'
+            valid.push({'posX': posX - 1, 'posY': posY + 1})
+          }
+        }
       }
-      if ( posX + 1 < 8 && posY - 1 >= 8 && !chessBoard[posX + 1][posY - 1]) { 
-        validMove.push({'posX': posX - 1, 'posY': posY - 1})
+      if ( posX + 1 < 8 && posY - 1 >= 8) {
+        if (!chessBoard[posX + 1][posY - 1]) {
+          ColorBoard[posX + 1][posY - 1] = '#696969'
+          valid.push({'posX': posX + 1, 'posY': posY - 1})
+        } else {
+          if (chessBoard[posX + 1][posY - 1][0] !== color) {
+            canEats.push({'posX': posX + 1, 'posY': posY - 1})
+            ColorBoard[posX + 1][posY - 1] = '#696969'
+            valid.push({'posX': posX + 1, 'posY': posY - 1})
+          }
+        }
       }
+      return {canEats, valid, ColorBoard}
     case 'Q': //Queen
-      return findStraight(posX, posY, chessBoard).push(...findDiagonal)
+      const valueStraight = findStraight(posX, posY, ColorBoard, chessBoard)
+      const valueDiagonal = findDiagonal(posX, posY, ColorBoard, chessBoard)
+      valid = valueStraight.valid.push(valueDiagonal.valid)
+      return {canEats, valid, ColorBoard}
     default:
-      break
+      return {canEats, valid, ColorBoard}
   }
 }
 
-const findStraight = (posX, posY, chessBoard) => {
-  validMove = []
+const findStraight = (posX, posY, ColorBoard, chessBoard, canEats, checkCheckmate) => {
+  
+  let valid = [{posX, posY}]
   let posXValidOn = posX
   let posXValidUnder = posX
   let posYValidLeft = posY
@@ -109,38 +298,45 @@ const findStraight = (posX, posY, chessBoard) => {
     if (!!chessBoard[posX - i][posY]) {
       break
     }
+    
     posXValidOn = posX - i
+    ColorBoard[posXValidOn][posY] = '#696969'
+    valid.push({'posX': posXValidOn, 'posY': posY})
   }
-  validMove.push({'posX': posXValidOn, 'posY': posY})
 
   for (let i = 1; i < 8 - posX; i++) {  // check xuong duoi
     if (!!chessBoard[posX + i][posY]) { 
       break
     }
     posXValidUnder = posX + i
+    ColorBoard[posXValidOn][posY] = '#696969'
+    valid.push({'posX': posXValidUnder, 'posY': posY})
   }
-  validMove.push({'posX': posXValidUnder, 'posY': posY})
 
   for (let i = 1; i <= posY; i++) { // check sang trai
     if (!!chessBoard[posX][posY - i]) { 
       break
     }
     posYValidLeft = posY - i
+    ColorBoard[posX][posYValidLeft] = '#696969'
+    valid.push({'posX': posX, 'posY': posYValidLeft})
   }
-  validMove.push({'posX': posX, 'posY': posYValidLeft})
+  
 
   for (let i = 1; i < 8 - posY; i++) { // check sang trai
     if (!!chessBoard[posX][posY + i]) { 
       break
     }
     posYValidRight = posY + i
+    ColorBoard[posX][posYValidRight] = '#696969'
+    valid.push({'posX': posX, 'posY': posYValidRight})
   }
-  validMove.push({'posX': posX, 'posY': posYValidRight})
 
-  return validMove
+  return {canEats, valid, ColorBoard}
 }
 
-const findDiagonal = (posX, posY, chessBoard) => {
+const findDiagonal = (posX, posY, ColorBoard, chessBoard, canEats) => {
+  let valid = [{posX, posY}]
   let tempX = posX - 1
   let tempY = posY - 1
   // check cheo 1
@@ -148,10 +344,11 @@ const findDiagonal = (posX, posY, chessBoard) => {
     if (!!chessBoard[tempX][tempY]) {
       break
     }
+    valid.push({'posX': tempX, 'posY': tempY})
+    ColorBoard[tempX][tempY] = '#696969'
     tempX--
     tempY--
   }
-  validMove.push({'posX': tempX, 'posY': tempY})
 
   tempX = posX + 1
   tempY = posY + 1
@@ -159,10 +356,11 @@ const findDiagonal = (posX, posY, chessBoard) => {
     if (!!chessBoard[tempX][tempY]) {
       break
     }
+    valid.push({'posX': tempX, 'posY': tempY})
+    ColorBoard[tempX][tempY] = '#696969'
     tempX++
     tempY++
   }
-  validMove.push({'posX': tempX, 'posY': tempY})
 
   tempX = posX - 1
   tempY = posY + 1
@@ -170,20 +368,36 @@ const findDiagonal = (posX, posY, chessBoard) => {
     if (!!chessBoard[tempX][tempY]) {
       break
     }
+    valid.push({'posX': tempX, 'posY': tempY})
+    ColorBoard[tempX][tempY] = '#696969'
     tempX--
     tempY++
   }
-  validMove.push({'posX': tempX, 'posY': tempY})
 
   tempX = posX + 1
   tempY = posY - 1
-  while (tempX >= 0 && tempY < 8) { // check cheo xuong ben trai
+  while (tempX < 8 && tempY >= 0) { // check cheo xuong ben trai
     if (!!chessBoard[tempX][tempY]) {
       break
     }
+    valid.push({'posX': tempX, 'posY': tempY})
+    ColorBoard[tempX][tempY] = '#696969'
     tempX++
     tempY--
   }
-  validMove.push({'posX': tempX, 'posY': tempY})
-  return validMove
+  return {canEats, valid, ColorBoard}
+}
+
+export const UpdateChessBoard = (from, to, color, piece, chessBoard) => {
+  const fromPosX = from.posX
+  const fromPosY = from.posY
+  const toPosX = to.posX
+  const toPosY = to.posY
+  chessBoard[fromPosX][fromPosY] = null
+  chessBoard[toPosX][toPosY] = `${color}${piece}`
+  return chessBoard
+}
+
+export const CheckCheckmate = (chessBoard, kingPosX, kingPosY, KingColor) => {
+  
 }
--- AppFood/src/screens/Game/Chess/ChessBoard.js
+++ AppFood/src/screens/Game/Chess/ChessBoard.js
@@ -1,59 +1,144 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Dimensions, TouchableWithoutFeedback} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5'
+import {View, Image, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
+import { FindValidRoad, UpdateChessBoard } from './Chess'
 
 export default class ChessBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      pieceChoosed: null,
+      validMoves: [],
       size: Dimensions.get('window').width / 8, // ChessBoard: 8x8
+      updateColorBoard: null,
+      turn: 'w',
+      ChessBoard: [
+        ['bR', 'bN', 'bB', 'bK', 'bQ', 'bB', 'bN', 'bR'],
+        ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+        ['wR', 'wN', 'wB', 'wK', 'wQ', 'wB', 'wN', 'wR'],
+      ]
     };
+    this.ColorBoard = [
+      ['#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863'],
+      ['#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5'],
+      ['#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863'],
+      ['#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5'],
+      ['#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863'],
+      ['#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5'],
+      ['#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863'],
+      ['#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5', '#b58863', '#f0d9b5'],
+    ]
+    this.urlPiece = {
+      'bR': require('../../../Assets/Image/bR.png'),
+      'bN': require('../../../Assets/Image/bN.png'),
+      'bB': require('../../../Assets/Image/bB.png'),
+      'bK': require('../../../Assets/Image/bK.png'),
+      'bQ': require('../../../Assets/Image/bQ.png'),
+      'bP': require('../../../Assets/Image/bP.png'),
+      'wR': require('../../../Assets/Image/wR.png'),
+      'wN': require('../../../Assets/Image/wN.png'),
+      'wB': require('../../../Assets/Image/wB.png'),
+      'wK': require('../../../Assets/Image/wK.png'),
+      'wQ': require('../../../Assets/Image/wQ.png'),
+      'wP': require('../../../Assets/Image/wP.png'),
+    }
     this.row = new Array(8).fill(null);
     this.column = new Array(8).fill(null);
-    this.ChessBoard = [
-      ['bR', 'bK', 'bB', 'bK', 'nQ', 'bB', 'bK', 'bR'],
-      ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null]
-      [null, null, null, null, null, null, null, null]
-      [null, null, null, null, null, null, null, null]
-      ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-      ['wR', 'wK', 'wB', 'wK', 'wQ', 'wB', 'wK', 'wR'],
-    ]
   }
+
+  pressSquare = (posX, posY) => {
+    const {pieceChoosed, ChessBoard, turn, validMoves} = this.state
+    const value = ChessBoard[posX][posY]
+    if (!value && !pieceChoosed) { // bấm lung tung
+      return
+    } else if (!!value) { // press piece
+      if (!!pieceChoosed) { // if !!picechoosed and your turn will eat enermey ( if value is enermy )
+        const fromPosX = pieceChoosed.posX
+        const fromPosY = pieceChoosed.posY
+        const fromValue = ChessBoard[fromPosX][fromPosY]
+        const fromColor = fromValue[0]
+        const fromPiece = fromValue[1]
+        if (fromColor !== value[0]){
+          const valid = validMoves.find(item => {
+            return item.posX === posX && item.posY === posY
+          })
+          if (!!valid) { // check enermy on valid moves of piecechoosed
+            let c = UpdateChessBoard(pieceChoosed, {posX, posY}, fromColor, fromPiece, ChessBoard)
+            this.setState({
+              pieceChoosed: null,
+              ChessBoard: c,
+              updateColorBoard: null,
+              turn: this.state.turn === 'w' ? 'b' : 'w'
+            })
+            return
+          }
+        }
+      }
+      // if not eats, so show valid moves
+      const color = value[0]
+      const piece = value[1]
+      this.setState({pieceChoosed: {posX, posY}})
+      let tempColor = new Array(8).fill(null)
+      for (let i = 0; i < 8; i++) {
+        tempColor[i] = [...this.ColorBoard[i]]
+      }
+      const finded = FindValidRoad(piece, {posX, posY}, tempColor, ChessBoard, color)
+      this.setState({ validMoves: finded.valid, updateColorBoard: finded.ColorBoard })
+    } else { // move piece
+      const fromPosX = pieceChoosed.posX
+      const fromPosY = pieceChoosed.posY
+      const value = ChessBoard[fromPosX][fromPosY]
+      const color = value[0]
+      const piece = value[1]
+      if (turn === color) { // move piece if piecechoosed is your turn
+        const valid = validMoves.find(item => {
+          return item.posX === posX && item.posY === posY
+        })
+        if (!!valid) {
+          let c = UpdateChessBoard(pieceChoosed, {posX, posY}, color, piece, ChessBoard)
+          this.setState({
+            pieceChoosed: null,
+            ChessBoard: c,
+            updateColorBoard: null,
+            turn: this.state.turn === 'w' ? 'b' : 'w'
+          })
+          return
+        }
+      }
+      this.setState({updateColorBoard: null, validMoves: []})
+    }
+  }
+
   render() {
-    const {size} = this.state;
+    const {size, ChessBoard, updateColorBoard} = this.state;
+    const color = !updateColorBoard ? this.ColorBoard : updateColorBoard
     return (
       <View style={styles.container}>
-        {this.row.map((c, idx_row) => {
+        {this.row.map((r, idx_row) => {
           return (
             <View key={idx_row} style={{flexDirection: 'row'}}>
-              {this.column.map((m, idx_column) => {
+              {this.column.map((c, idx_column) => {
                 return (
-                  <TouchableWithoutFeedback
+                  <TouchableOpacity
+                    onPress={() => this.pressSquare(idx_row, idx_column)}
                     key={idx_column}
                     style={{
-                      backgroundColor:
-                        idx_column % 2 === 0
-                          ? idx_row % 2 === 0
-                            ? '#f0d9b5'
-                            : '#b58863'
-                          : idx_row % 2 !== 0
-                          ? '#f0d9b5'
-                          : '#b58863',
+                      backgroundColor: color[idx_row][idx_column],
                       width: size,
                       justifyContent: 'center',
                       alignItems: 'center',
                       height: size,
                     }}>
-                    {(idx_row === 1 || idx_row === 6) && (
-                      <Icon name="chess-pawn" size={size * 0.7} />
-                    )}
-                    {(idx_column === 0 || idx_column === 7) &&
-                      (idx_row === 0 || idx_row === 7) && (
-                        <Icon name="chess-rook" size={size * 0.7} />
-                      )}
-                  </TouchableWithoutFeedback>
+                    {ChessBoard[idx_row][idx_column] ?
+                      <Image
+                        style={{width: 0.7*size, height: 0.7*size}}
+                        source={this.urlPiece[ChessBoard[idx_row][idx_column]]} />
+                      : null}
+                  </TouchableOpacity>
                 );
               })}
             </View>
